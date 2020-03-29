63490e3babd86c130f480068b4243bd8c230c126
hhh123123123 <601425283@qq.com>
2020-03-30 00:23:53 +0800

    improve some function

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
-from machine import Pin, I2C, Timer, UART
-from micropython import mem_info
 import response
+import dataProcess
+
+import micropython
+import machine
 import urequests
 import _thread
 import network
 import socket
 import ssd1306
-import json
 import time
-import dht
-import re
 
-mem_info()
+import esp
+import re
 
 ip = "0.0.0.0"
 mask = "0.0.0.0"
 gw = "0.0.0.0"
 dns = "0.0.0.0"
-timeout = False
 ap = network.WLAN(network.AP_IF)
 station = network.WLAN(network.STA_IF)
 
-humidity = 0
-temperature = 0
-dht11 = dht.DHT11(Pin(25))
-
-i2c = I2C(scl=Pin(15), sda=Pin(4), freq=100000)
+i2c = machine.I2C(scl=machine.Pin(15), sda=machine.Pin(4), freq=100000)
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
-
-jsondoc = {
-    'Sid_Pwd': [{'ssid': 'example', 'password': 'yourpassword'}]
-}
-
-Air = {"PM1_0CF1": "31", "PM2_5CF1": "46", "PM10CF1": "58", "PM1_0AE": "34", "PM2_5AE": "43", "PM10AE": "48",
-       "Gt0_3um": "1620", "Gt0_5um": "288", "Gt1_0um": "32", "Gt2_5um"
-       : "9", "Gt5_0um": "2", "Gt10um": "1"}
 
 
 def checkwifi(tim0):
@@ -43,17 +30,16 @@ def checkwifi(tim0):
 
 
 def refresh_oled(tim1):
-    oled.fill(0)
-    dht11.measure()
-    global humidity
-    global temperature
-    humidity = dht11.humidity()
-    temperature = dht11.temperature()
-    oled.text("I" + ip, 0, 0, col=1)
-    oled.text("G" + gw, 0, 9, col=1)
-    oled.text("Humidity:" + str(humidity) + "%", 0, 18, col=1)
-    oled.text("Temperature:" + str(temperature) + "'C", 0, 27, col=1)  # Celsius ℃
-    oled.show()
+    h, t = dataProcess.releasedht()
+    try:
+        oled.fill(0)
+        oled.text("I" + ip, 0, 0, col=1)
+        oled.text("G" + gw, 0, 9, col=1)
+        oled.text("Humidity:" + str(h) + "%", 0, 18, col=1)
+        oled.text("Temperature:" + str(t) + "'C", 0, 27, col=1)  # Celsius ℃
+        oled.show()
+    except:
+        print("error in oled")
 
 
 def configwifi(sid, pwd):
@@ -78,6 +64,17 @@ def configwifi(sid, pwd):
     return False
 
 
+def firstload():
+    dataProcess.rwjson(True)  # 读取json文件里的数据
+    station.active(True)
+    ssids = station.scan()
+    for i in dataProcess.jsondoc['Sid_Pwd']:
+        for j in ssids:
+            if j[0].decode() == i['ssid']:
+                if configwifi(i['ssid'], i['password']):
+                    return
+
+
 def webserver():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -91,7 +88,6 @@ def webserver():
             route = request[0].split(' ')
             print(address, client)
             print(route[1])  # request_url
-            # client.sendall('<script>alert("Welcome!");</script>')
 
             if route[1] == '/':
                 client.sendall(response.header200())
@@ -103,7 +99,7 @@ def webserver():
                     client.sendall('<br>')
 
             elif re.match(r'/\?ssid=(.*?)&pwd=(.*)', route[1]):
-                obj1 = re.match(r'/\?ssid=(.*?)&pwd=(.*)', route[1])   # sid=obj.group(1);pwd=obj.group(2)
+                obj1 = re.match(r'/\?ssid=(.*?)&pwd=(.*)', route[1])  # sid=obj.group(1);pwd=obj.group(2)
 
                 client.sendall(response.header200())
                 client.sendall("<p>Waiting......</p>")
@@ -116,89 +112,71 @@ def webserver():
 
                 if configwifi(obj1.group(1), obj1.group(2)):
                     client.sendall("<p>Connected to {0} </p>".format(obj1.group(1)))
-                    savekey(obj1.group(1), obj1.group(2))
+                    dataProcess.savekey(obj1.group(1), obj1.group(2))
                 else:
                     client.sendall("<p>Fail......</p>")
 
-            elif route[1] == '/api':
+            elif route[1] == '/api/air':
                 client.sendall(response.header200('json'))
-                client.sendall(json.dumps(Air))
+                client.sendall(dataProcess.String_Air)
+
+            elif route[1] == '/api/gps':
+                client.sendall(response.header200('json'))
+                client.sendall(dataProcess.String_GPS)
+
+            elif route[1] == '/favicon.ico':
+                client.sendall(response.Icon200)
+                with open('favicon.ico', "r") as f:
+                    client.sendall(f.read())
+                    f.close()
 
             else:
                 client.sendall(response.header404())
-
             client.close()
 
-        except OSError as e:
-            print(e)
-
-
-def rwjson(c):
-    global jsondoc
-    try:
-        if c:
-            with open('wificonfig.json', "r") as f:
-                jsondoc = json.loads(f.read())
-            f.close()
-        else:
-            with open('wificonfig.json', "w") as f:
-                f.write(json.dumps(jsondoc))
-            f.close()
-    except:
-        pass
-
-
-def firstload():
-    global jsondoc
-    rwjson(True)  # 读取json文件里的数据
-    station.active(True)
-    ssids = station.scan()
-    for i in jsondoc['Sid_Pwd']:
-        for j in ssids:
-            if j[0].decode() == i['ssid']:
-                if configwifi(i['ssid'], i['password']):
-                    return
-
-
-def savekey(sid, pwd):
-    global jsondoc
-    for i in jsondoc['Sid_Pwd']:
-        if sid == i['ssid']:
-            i['password'] = pwd
-            rwjson(False)  # 写入json
-            return
-    jsondoc['Sid_Pwd'].append({'ssid': sid, 'password': pwd})
-    rwjson(False)
+        except:
+            client.close()
+            print("error in webserver")
 
 
 def analysis():
     while True:
         if (uart2.any()):
             rec = uart2.read().decode()
-            # print(rec)
-        time.sleep(0.05)  # 不加延迟且串口没有收到数据的时候会卡死
+            dataProcess.releasepack(rec)
+
+        time.sleep_ms(50)  # 不加延迟且串口没有收到数据的时候会卡死
 
 
 if __name__ == '__main__':
-    tim0 = Timer(0)
-    tim1 = Timer(1)
-    tim0.init(period=50000, mode=Timer.PERIODIC, callback=checkwifi)  # 检测WiFi是否断线,防止无限重连
-    tim1.init(period=1000, mode=Timer.PERIODIC, callback=refresh_oled)  # 一秒刷新一次oled
+    tim0 = machine.Timer(0)
+    tim1 = machine.Timer(1)
+    # wdt = machine.WDT(timeout=30000)#看门dog
+    # machine.reset()重启
+    # wdt.feed()
+    tim0.init(period=50000, mode=machine.Timer.PERIODIC, callback=checkwifi)  # 检测WiFi是否断线,防止无限重连
+    tim1.init(period=1000, mode=machine.Timer.PERIODIC, callback=refresh_oled)  # 一秒刷新一次oled
     # uart1 = UART(1, baudrate=115200, bits=8, rx=9, tx=10, stop=1, timeout=10)   # 串口1串口2，还有一个串口被micropython使用
-    uart2 = UART(2, baudrate=115200, bits=8, rx=16, tx=17, stop=1, timeout=10)
+    uart2 = machine.UART(2, baudrate=115200, bits=8, rx=16, tx=17, stop=1, timeout=10)
 
     firstload()  # sta加载json文件并自动连接WiFi
 
     ap.active(True)
     ap.config(essid="ESP32-Webconfig", authmode=4, password="12345678")  # authmode=network.AUTH_WPA_WPA2_PSK=4
 
-    lock = _thread.allocate_lock()
+    # lock = _thread.allocate_lock()
+
+    print(esp.flash_size())
+    print(esp.flash_user_start())
+    micropython.mem_info()
+    # machine.freq(160000000)
+    machine.freq()
 
     _thread.start_new_thread(webserver, ())  # 多线程运行webserver
-    _thread.start_new_thread(analysis, ()) # 接受串口数据
+    # _thread.start_new_thread(analysis, ())  # 接受串口数据
 
-    # while True:
-    #     if (uart2.any()):
-    #         rec = uart2.read().decode()
-    #         # print(rec)
-    #     time.sleep(0.05)  # 不加延迟且串口没有收到数据的时候会卡死
+    while True:
+        if (uart2.any()):
+            rec = uart2.read().decode()
+            dataProcess.releasepack(rec)
+        time.sleep_ms(50)  # 不加延迟且串口没有收到数据的时候会卡死
--- response.py
+++ response.py
@@ -1,7 +1,15 @@
+Icon200='''
+HTTP/1.1 200 OK
+accept-ranges: bytes
+content-type: image/x-icon
+Access-Control-Allow-Origin: *
+Server: ESP32-Webserver
+
+'''
 
 OK200 = '''
 HTTP/1.1 200 OK
-Content-Type: {0};charset=utf-8
+Content-Type: {0};charset={1}
 Access-Control-Allow-Origin: *
 Server: ESP32-Webserver
 
@@ -19,25 +27,26 @@ Server: ESP32-Webserver
 Welcome to->MY GITHUB</a>
 '''
 
-dType={
-    'html':'text/html',
-    'css':'text/css',
-    'javascript':'application/javascript',
-    'json':'application/json',
+dType = {
+    'html': 'text/html',
+    'css': 'text/css',
+    'javascript': 'application/javascript',
+    'json': 'application/json',
+    'icon': 'image/x-icon'
 }
 
 
-def header200(tp='html'):
+def header200(tp='html', charset='utf-8'):
     for i in dType.keys():
-        if tp==i:
-            return OK200.format(dType[tp])
+        if tp == i:
+            return OK200.format(dType[tp], charset)
         else:
-            return OK200.format(dType['html'])
+            return OK200.format(dType['html'], charset)
 
 
 def header404(tp='html'):
     for i in dType.keys():
-        if tp==i:
+        if tp == i:
             return NotFound404.format(dType[tp])
         else:
             return NotFound404.format(dType['html'])
