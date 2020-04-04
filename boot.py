1d0ad455e5e8cca66746343e6920a32e9544e420
windfallw <601425283@qq.com>
2020-04-05 00:05:39 +0800

    first load

---
+++ boot.py
@@ -0,0 +1,15 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+import micropython
+import machine
+import webrepl
+import esp
+import gc
+
+micropython.alloc_emergency_exception_buf(100)  # 设置紧急情况下的（栈溢出，普通RAM不足等）保险RAM分配，使在紧急情况下仍有RAM可用。
+machine.freq(160000000)
+webrepl.start()
+esp.osdebug(None)
+gc.enable()
+
+print(machine.freq())
+print("flash_size: {0}, flash_user_start: {1}.".format(esp.flash_size(),esp.flash_user_start()))
