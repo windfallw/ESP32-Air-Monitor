a15529391f5693e377b128ffe1dd60d93101574d
windfallw <601425283@qq.com>
2020-04-12 23:45:54 +0800

    cpu frq

--- boot.py
+++ boot.py
@@ -6,7 +6,8 @@ import esp
 import gc
 
 micropython.alloc_emergency_exception_buf(100)  # 设置紧急情况下的（栈溢出，普通RAM不足等）保险RAM分配，使在紧急情况下仍有RAM可用。
-machine.freq(160000000)
+# micropython.mem_info()
+machine.freq(240000000)
 webrepl.start()
 esp.osdebug(None)
 gc.enable()
