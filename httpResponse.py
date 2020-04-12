16b2b4fdd4343e5d2dd8a87faf6cbd8034ca4f55
windfallw <601425283@qq.com>
2020-04-12 23:47:25 +0800

    make some change

--- httpResponse.py
+++ httpResponse.py
@@ -1,5 +1,5 @@
 Icon200='''
-HTTP/1.1 200 OK
+HTTP/1.0 200 OK
 accept-ranges: bytes
 content-type: image/x-icon
 Access-Control-Allow-Origin: *
@@ -8,16 +8,16 @@ Server: ESP32-Webserver
 '''
 
 OK200 = '''
-HTTP/1.1 200 OK
-Content-Type: {0};charset={1}
+HTTP/1.0 200 OK
+Content-Type: {0};charset=utf-8
 Access-Control-Allow-Origin: *
 Server: ESP32-Webserver
 
 '''
 
 NotFound404 = '''
-HTTP/1.1 404 NOT FOUND
-Content-Type: {0};charset=utf-8
+HTTP/1.0 404 NOT FOUND
+Content-Type: text/html;charset=utf-8
 Access-Control-Allow-Origin: *
 Server: ESP32-Webserver
 
@@ -36,16 +36,8 @@ dType = {
 }
 
 
-def header200(tp='html', charset='utf-8'):
+def header200(tp='html'):
     for i in dType.keys():
         if tp == i:
-            return OK200.format(dType[tp], charset)
-    return OK200.format(dType['html'], charset)
-
-
-def header404(tp='html'):
-    for i in dType.keys():
-        if tp == i:
-            return NotFound404.format(dType[tp])
-        else:
-            return NotFound404.format(dType['html'])
+            return OK200.format(dType[tp])
+    return OK200.format(dType['html'])
