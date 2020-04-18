178eebe0bca23af37d9a3e49f91cd5a0529f3c0e
windfallw <601425283@qq.com>
2020-04-18 17:55:47 +0800

    现在可以使用@装饰器来定义web服务器路由路径啦，更加的简洁了，i了这就是Python吗

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import tools
-import httpResponse
-import httpRequest
+import Webserver
+import Webclient
 import ssd1306
 import _thread
 import network
-import socket
 import machine
 import time
-import gc
 import re
 
 ip = "0.0.0.0"
@@ -66,144 +64,100 @@ def firstload():
     tools.rwjson(True)  # 读取json文件里的数据
     station.active(True)
     ssids = station.scan()
-    for i in tools.conf['Sid_Pwd']:
+    for i in tools.conf['network']:
         for j in ssids:
             if j[0].decode() == i['ssid']:
                 if configwifi(i['ssid'], i['password']):
                     return
 
 
-def Webserver():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('0.0.0.0', 80))
-    s.listen(1)
-    print("listen on 80.")
-    while True:
-        try:
-            client, address = s.accept()
-            request = client.recv(1024).decode().split('\r\n', 1)
-            route = request[0].split(' ', 2)
-            # print(request)
-            # print(route)
-            print(address, client)
-            print(route[1])  # request_url
+app = Webserver.WebServant()
 
-            if route[0].upper() == 'POST':
-                postdata = request[1].rsplit('\r\n', 1)[-1]
-                print(postdata)
 
-                if route[1] == '/postwifi':
-                    client.send(httpResponse.header200())
-                    client.send("<p>连接中......</p>")
-                    obj1 = re.match(r'ssid=(.*?)&pwd=(.*)', postdata)
-                    ssid, pwd = obj1.group(1), obj1.group(2)
-                    if not re.match('192.168.4', address[0]):
-                        client.send("<p>当前访问IP是 %s ,WIFI连接过程中将中断Web通信,连接结果看OLED屏</p>" % (address[0]))
-                        client.close()
-                        configwifi(ssid, pwd)
-                        continue
-                    if configwifi(ssid, pwd):
-                        client.send("<p>成功连接到 %s </p>" % (obj1.group(1)))
-                        tools.savewifi(obj1.group(1), obj1.group(2))
-                    else:
-                        client.send("<p>连接失败......the pwd is %s</p>" % (pwd))
+@app.route('/')
+def index(*function):
+    client, address = function
+    client.send(Webserver.header200())
+    with open('/www/index.html', 'rb') as f:
+        line = f.read(8192)
+        while line:
+            client.send(line)
+            line = f.read(8192)
+        f.close()
+    sids = station.scan()
+    for i in sids:
+        client.send(i[0])
+        client.send('<br>')
 
-                if route[1] == '/posthost':
-                    client.send(httpResponse.header200())
-                    client.send("<p>正在设置中......</p>")
-                    obj2 = re.match(r'Host=(.*?)&Port=(.*)', postdata)
-                    host, port = obj2.group(1), int(obj2.group(2))
-                    tools.saverequester(host, port)
-                    client.send("<p>设置完毕......已自动重启</p>")
-                    client.close()
-                    machine.reset()
 
-            elif route[0].upper() == 'GET':
+@app.route('/info/air')
+def infoair(*function):
+    client, address = function
+    client.send(Webserver.header200('json'))
+    client.send(tools.PMS7003)
 
-                if route[1] == '/':
-                    client.send(httpResponse.header200())
-                    with open('/www/index.html', 'rb') as html:
-                        client.send(html.read())
-                    sids = station.scan()
-                    for i in sids:
-                        client.send(i[0])
-                        client.send('<br>')
 
-                elif route[1] == '/info/air':
-                    client.send(httpResponse.header200('json'))
-                    client.send(tools.String_Air)
+@app.route('/info/gps')
+def infogps(*function):
+    client, address = function
+    client.send(Webserver.header200('json'))
+    client.send(tools.GPS)
 
-                elif route[1] == '/info/gps':
-                    client.send(httpResponse.header200('json'))
-                    client.send(tools.String_GPS)
 
-                elif route[1] == '/favicon.ico':
-                    client.send(httpResponse.Icon200)
-                    with open('/www/favicon.ico', 'rb') as f:
-                        line = f.read(8192)
-                        while line:
-                            client.send(line)
-                            line = f.read(8192)
-                        f.close()
+@app.route('/favicon.ico')
+def favoicon(*function):
+    client, address = function
+    client.send(Webserver.Icon200)
+    with open('/www/favicon.ico', 'rb') as f:
+        line = f.read(8192)
+        while line:
+            client.send(line)
+            line = f.read(8192)
+        f.close()
 
-                elif route[1] == '/src/bootstrap.min.css':
-                    client.send(httpResponse.header200('css'))
-                    with open('/src/bootstrap.min.css', 'rb') as f:
-                        line = f.read(8192)
-                        while line:
-                            client.send(line)
-                            line = f.read(8192)
-                        f.close()
 
-                elif route[1] == '/src/bootstrap.bundle.min.js':
-                    client.send(httpResponse.header200('javascript'))
-                    with open('/src/bootstrap.bundle.min.js', 'rb') as f:
-                        line = f.read(8192)
-                        while line:
-                            client.send(line)
-                            line = f.read(8192)
-                        f.close()
+@app.route('/postwifi', 'POST')
+def postwifi(*function):
+    client, address, client_data = function
+    client.send(Webserver.header200())
+    client.send("<p>连接中......</p>")
+    obj1 = re.match(r'ssid=(.*?)&pwd=(.*)', client_data)
+    ssid, pwd = obj1.group(1), obj1.group(2)
+    if not re.match('192.168.4', address[0]):
+        client.send("<p>当前访问IP是 %s ,WIFI连接过程中将中断Web服务,连接结果看OLED屏</p>" % (address[0]))
+        client.close()
+        configwifi(ssid, pwd)
+        return
+    if configwifi(ssid, pwd):
+        client.send("<p>成功连接到 %s </p>" % (obj1.group(1)))
+        tools.savewifi(obj1.group(1), obj1.group(2))
+    else:
+        client.send("<p>连接失败......the pwd is %s</p>" % (pwd))
 
-                elif route[1] == '/src/echarts.min.js':
-                    client.send(httpResponse.header200('javascript'))
-                    with open('/src/echarts.min.js', 'rb') as f:
-                        line = f.read(8192)
-                        while line:
-                            client.send(line)
-                            line = f.read(8192)
-                        f.close()
 
-                elif route[1] == '/src/jquery.min.js':
-                    client.send(httpResponse.header200('javascript'))
-                    with open('/src/jquery.min.js', 'rb') as f:
-                        line = f.read(8192)
-                        while line:
-                            client.send(line)
-                            line = f.read(8192)
-                        f.close()
-
-            else:
-                client.send(httpResponse.NotFound404)
-
-        except Exception as e:
-            print('WEBSrv:', e)
-
-        finally:
-            client.close()
-            gc.collect()
-            print('use:', gc.mem_alloc(), 'remain:', gc.mem_free())
+@app.route('/posthost', 'POST')
+def posthost(*function):
+    client, address, client_data = function
+    client.send(Webserver.header200())
+    client.send("<p>正在设置中......</p>")
+    obj2 = re.match(r'Host=(.*?)&Port=(.*)', client_data)
+    host, port = obj2.group(1), int(obj2.group(2))
+    tools.savehttpRequest(host, port)
+    client.send("<p>设置完毕......已自动重启</p>")
+    client.close()
+    machine.reset()
 
 
 def UART2():
-    r = httpRequest.Requester(tools.conf['Requester'])
+    uart2 = machine.UART(2, baudrate=115200, bits=8, rx=16, tx=17, stop=1, timeout=10)
+    r = Webclient.HttpRequest(tools.conf['httpRequest'])
     while True:
         try:
-            if (uart2.any()):
-                rec = uart2.readline().decode()
-                r.do(tools.releasepack(rec))
+            if uart2.any():
+                UartRecv = uart2.readline().decode()
+                r.do(tools.releasepack(UartRecv))
         except Exception as e:
-            print(rec.encode())
+            print(UartRecv.encode())
             print('UART2:', e)
         time.sleep_ms(50)  # 不加延迟且串口没有收到数据的时候会卡死
 
@@ -217,12 +171,12 @@ if __name__ == '__main__':
     tim0.init(period=60000, mode=machine.Timer.PERIODIC, callback=checkwifi)  # 检测WiFi是否断线,防止无限重连
     tim1.init(period=3000, mode=machine.Timer.PERIODIC, callback=refresh_oled)  # 3秒刷新一次oled
     # uart1 = UART(1, baudrate=115200, bits=8, rx=9, tx=10, stop=1, timeout=10)   # 串口1串口2，还有一个串口被micropython使用
-    uart2 = machine.UART(2, baudrate=115200, bits=8, rx=16, tx=17, stop=1, timeout=10)
 
     firstload()  # sta加载json文件并自动连接WiFi
 
     ap.active(True)
     ap.config(essid="ESP32-Webconfig", authmode=4, password="12345678")  # authmode=network.AUTH_WPA_WPA2_PSK=4
-
-    _thread.start_new_thread(Webserver, ())  # 多线程运行webserver
+    print(app.route_table_get)
+    print(app.route_table_post)
+    _thread.start_new_thread(app.run, ('0.0.0.0', 80))  # 多线程运行webserver
     _thread.start_new_thread(UART2, ())  # 接受串口数据
