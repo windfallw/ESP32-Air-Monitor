ef2fece0548885f0358de5e1cdf20bc0f528ae70
windfallw <601425283@qq.com>
2020-04-19 16:14:22 +0800

    修复了一个很严重的bug，自动添加静态文件和html文件到路由表的功能更加完善了。

--- Webserver.py
+++ Webserver.py
@@ -39,50 +39,72 @@ Server: ESP32-Webserver
 
 '''
 
-dType = {
+data_type = {
     'html': 'text/html',
     'css': 'text/css',
     'js': 'application/javascript',
     'json': 'application/json',
-    'icon': 'image/x-icon'
+    'ico': 'image/x-icon'
 }
 
 
-def header200(type='html'):
-    "传入响应头的文件类型"
-    for i in dType.keys():
-        if type == i:
-            return OK200.format(dType[type])
-    return OK200.format(dType['html'])
+def header200(data_type_key='html'):
+    """传入响应头的文件类型"""
+    for i in data_type.keys():
+        if data_type_key == i:
+            return OK200.format(data_type[data_type_key])
+    return OK200.format(data_type['html'])
 
 
 class WebServant:
     route_table_get = []
     route_table_post = []
+    autoload_allow_staticfiletype = ['js', 'css']
 
-    def __init__(self):
-        "init初始化时会自动添加src目录下的js和css文件在get路由表中"
-        os.chdir('src')
-        file = os.listdir()
-        for i in file:
-            type = i.rsplit('.')[-1]
-            path = '/src/' + i
+    def __init__(self, static_path=None, template_path=None):
+        """
+        init初始化时可自动添加指定目录下的html或js css文件在get路由表中, 指定目录请使用绝对路径.
+        static_path文件夹中请放js和css,template_path放html.
+        """
+        if static_path:
+            os.chdir(static_path)
+            static_file = os.listdir()
+            for i in static_file:
+                type = i.rsplit('.')[-1]
+                if type not in self.autoload_allow_staticfiletype:
+                    continue
+                file_path = static_path + '/' + i
+                self.add_file_to_route(file_path, file_path, type)
 
-            def static_file(*function):
-                client, address = function
-                client.send(header200(type))
-                with open(path, 'rb') as f:
+        if template_path:
+            os.chdir('..' + template_path)
+            template_file = os.listdir()
+            for i in template_file:
+                type = i.rsplit('.')[-1]
+                if type == 'html':
+                    file_path = template_path + '/' + i
+                    route_path = '/' + i
+                    self.add_file_to_route(file_path, route_path, type)
+
+        os.chdir('/')  # 读取完文件后必须回到根目录否则main.py的函数无法正常加载
+        gc.collect()
+
+    def add_file_to_route(self, file_path, route_path, type):
+        def file_function(*function, p=file_path, t=type):
+            client, address = function
+            client.send(header200(t))
+            with open(p, 'rb') as f:
+                line = f.read(8192)
+                while line:
+                    client.send(line)
                     line = f.read(8192)
-                    while line:
-                        client.send(line)
-                        line = f.read(8192)
-                    f.close()
+                f.close()
+            client.close()
 
-            self.route_table_get.append([path, static_file])
-        os.chdir('/')
+        self.route_table_get.append([route_path, file_function])
 
     def route(self, path, method='GET'):
-        "装饰器,方法可选择get或者post,默认为get"
+        """装饰器,方法可选择get或者post,默认为get"""
 
         def decorator(func):
             if method.upper() == 'GET':
@@ -96,12 +118,12 @@ class WebServant:
         return decorator
 
     def run(self, host='0.0.0.0', port=80):
-        "host和port默认为本机ip的80端口"
+        """host和port默认为本机ip的80端口"""
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((host, port))
-        sock.listen(1)
-        print("listen on 80.")
+        sock.listen(5)
+        print("listen on %s." % port)
         while True:
             try:
                 client, address = sock.accept()
