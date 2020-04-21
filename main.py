712015f95a20ae076f3a4882ef2a7e7413ea3622
windfallw <601425283@qq.com>
2020-04-21 16:45:46 +0800

    将WiFi连接 oled 文件读写等功能写成类集成于tools.py

--- main.py
+++ main.py
@@ -1,82 +1,17 @@
 import tools
 import Webserver
 import Webclient
-import ssd1306
 import _thread
-import network
 import machine
 import time
 import re
-
-ip = "0.0.0.0"
-mask = "0.0.0.0"
-gw = "0.0.0.0"
-dns = "0.0.0.0"
-ap = network.WLAN(network.AP_IF)
-station = network.WLAN(network.STA_IF)
-
-i2c = machine.I2C(scl=machine.Pin(15), sda=machine.Pin(4), freq=100000)
-oled = ssd1306.SSD1306_I2C(128, 64, i2c)
-
-
-def checkwifi(tim0):
-    if not station.isconnected():
-        station.disconnect()
-    # if ap.isconnected()
-
-
-def refresh_oled(tim1):
-    h, t = tools.releasedht()
-    try:
-        oled.fill(0)
-        oled.text("I" + ip, 0, 0, col=1)
-        oled.text("G" + gw, 0, 9, col=1)
-        oled.text("Humidity:" + str(h) + "%", 0, 18, col=1)
-        oled.text("Temperature:" + str(t) + "'C", 0, 27, col=1)  # Celsius ℃
-        oled.show()
-    except Exception as e:
-        print('OLED:', e)
-
-
-def configwifi(sid, pwd):
-    global station, ap, ip, mask, gw, dns
-    station.disconnect()
-    station.connect(sid, pwd)
-    try:
-        for s in range(0, 50):  # i 0-49
-            if not station.isconnected():
-                time.sleep(0.1)
-            else:
-                print("Connection successful")
-                # print(station.ifconfig())
-                return True
-    finally:
-        ip = station.ifconfig()[0]
-        mask = station.ifconfig()[1]
-        gw = station.ifconfig()[2]
-        dns = station.ifconfig()[3]
-    station.disconnect()
-    print("out of 5s connect fail......")
-    return False
-
-
-def firstload():
-    tools.rwjson(True)  # 读取json文件里的数据
-    station.active(True)
-    ssids = station.scan()
-    for i in tools.conf['network']:
-        for j in ssids:
-            if j[0].decode() == i['ssid']:
-                if configwifi(i['ssid'], i['password']):
-                    return
-
 
 app = Webserver.WebServant(static_path='/src', template_path='/www')
 
 
 @app.route('/')
-def index(*function):
-    client, address = function
+def index(*arguments):
+    client, address = arguments
     client.send(Webserver.header200())
     with open('/www/index.html', 'rb') as f:
         line = f.read(8192)
@@ -91,22 +26,22 @@ def index(*function):
 
 
 @app.route('/info/air')
-def infoair(*function):
-    client, address = function
+def infoair(*arguments):
+    client, address = arguments
     client.send(Webserver.header200('json'))
     client.send(tools.PMS7003)
 
 
 @app.route('/info/gps')
-def infogps(*function):
-    client, address = function
+def infogps(*arguments):
+    client, address = arguments
     client.send(Webserver.header200('json'))
     client.send(tools.GPS)
 
 
 @app.route('/favicon.ico')
-def favoicon(*function):
-    client, address = function
+def favoicon(*arguments):
+    client, address = arguments
     client.send(Webserver.Icon200)
     with open('/www/favicon.ico', 'rb') as f:
         line = f.read(8192)
@@ -117,8 +52,8 @@ def favoicon(*function):
 
 
 @app.route('/postwifi', 'POST')
-def postwifi(*function):
-    client, address, client_data = function
+def postwifi(*arguments):
+    client, address, client_data = arguments
     client.send(Webserver.header200())
     client.send("<p>连接中......</p>")
     obj1 = re.match(r'ssid=(.*?)&pwd=(.*)', client_data)
@@ -126,18 +61,18 @@ def postwifi(*function):
     if not re.match('192.168.4', address[0]):
         client.send("<p>当前访问IP是 %s ,WIFI连接过程中将中断Web服务,连接结果看OLED屏</p>" % (address[0]))
         client.close()
-        configwifi(ssid, pwd)
+        wifi_manage.configwifi(ssid, pwd)
         return
-    if configwifi(ssid, pwd):
-        client.send("<p>成功连接到 %s </p>" % (obj1.group(1)))
-        tools.savewifi(obj1.group(1), obj1.group(2))
+    if wifi_manage.configwifi(ssid, pwd):
+        client.send("<p>成功连接到 %s </p>" % ssid)
+        wifi_manage.savewifi(ssid, pwd)
     else:
-        client.send("<p>连接失败......the pwd is %s</p>" % (pwd))
+        client.send("<p>连接失败......你输入的密码是 %s</p>" % (pwd))
 
 
 @app.route('/posthost', 'POST')
-def posthost(*function):
-    client, address, client_data = function
+def posthost(*arguments):
+    client, address, client_data = arguments
     client.send(Webserver.header200())
     client.send("<p>正在设置中......</p>")
     obj2 = re.match(r'Host=(.*?)&Port=(.*)', client_data)
@@ -148,14 +83,18 @@ def posthost(*function):
     machine.reset()
 
 
+def UART1():
+    uart1 = machine.UART(1, baudrate=115200, bits=8, rx=9, tx=10, stop=1, timeout=10)
+
+
 def UART2():
     uart2 = machine.UART(2, baudrate=115200, bits=8, rx=16, tx=17, stop=1, timeout=10)
-    r = Webclient.HttpRequest(tools.conf['httpRequest'])
+    request = Webclient.HttpRequest(tools.config['httpRequest_config'])
     while True:
         try:
             if uart2.any():
                 UartRecv = uart2.readline().decode()
-                r.do(tools.releasepack(UartRecv))
+                request.do(tools.releasepack(UartRecv))
         except Exception as e:
             print(UartRecv.encode())
             print('UART2:', e)
@@ -163,19 +102,13 @@ def UART2():
 
 
 if __name__ == '__main__':
-    tim0 = machine.Timer(0)
-    tim1 = machine.Timer(1)
-    # wdt = machine.WDT(timeout=30000)#看门dog
-    # machine.reset()重启
-    # wdt.feed()
-    tim0.init(period=60000, mode=machine.Timer.PERIODIC, callback=checkwifi)  # 检测WiFi是否断线,防止无限重连
-    tim1.init(period=3000, mode=machine.Timer.PERIODIC, callback=refresh_oled)  # 3秒刷新一次oled
-    # uart1 = UART(1, baudrate=115200, bits=8, rx=9, tx=10, stop=1, timeout=10)   # 串口1串口2，还有一个串口被micropython使用
+    oled_manage = tools.Oled()
+    oled_manage.tim1.init(period=2000, mode=machine.Timer.PERIODIC,
+                          callback=lambda t: oled_manage.refresh_oled(wifi_manage.network_config))  # 2秒刷新一次oled
+    wifi_manage = tools.WiFi()
+    wifi_manage.tim0.init(period=60000, mode=machine.Timer.PERIODIC,
+                          callback=lambda t: wifi_manage.checkwifi())  # 检测WiFi是否断线,防止无限重连
 
-    firstload()  # sta加载json文件并自动连接WiFi
-
-    ap.active(True)
-    ap.config(essid="ESP32-Webconfig", authmode=4, password="12345678")  # authmode=network.AUTH_WPA_WPA2_PSK=4
     print(app.route_table_get)
     print(app.route_table_post)
     _thread.start_new_thread(app.run, ('0.0.0.0', 80))  # 多线程运行webserver
--- tools.py
+++ tools.py
@@ -1,53 +1,131 @@
-from machine import Pin
+import machine
+import network
+import ssd1306
 import json
+import time
 import dht
 
+# wdt = machine.WDT(timeout=30000)#看门dog
+# machine.reset()重启
+# wdt.feed()
 humidity = 0
 temperature = 0
-dht11 = dht.DHT11(Pin(25))
+dht11 = dht.DHT11(machine.Pin(25))
 
-httpRequestDir = {'Proto': 'http', 'host': 'www.example.com', 'Port': 80, 'method': 'POST',
-                  'Path': {'pms7003': '/info/sendair', 'gps': '/info/sendgps', 'voc': '/info/sendvoc'}}
-conf = {
+httpRequest_config = {'Proto': 'http', 'host': 'www.example.com', 'Port': 80, 'method': 'POST',
+                      'Path': {'pms7003': '/info/sendair', 'gps': '/info/sendgps', 'voc': '/info/sendvoc'}}
+config = {
     'network': [{'ssid': 'example', 'password': 'yourpassword'}],
-    'httpRequest': httpRequestDir
+    'httpRequest_config': httpRequest_config
 }
 
 PMS7003 = ''
 GPS = ''
 
 
+class WiFi:
+    ip = "0.0.0.0"
+    mask = "0.0.0.0"
+    gw = "0.0.0.0"
+    dns = "0.0.0.0"
+    network_config = (ip, mask, gw, dns)
+    ap = network.WLAN(network.AP_IF)
+    station = network.WLAN(network.STA_IF)
+    tim0 = machine.Timer(0)
+
+    def __init__(self):
+        self.firstload()
+        self.ap.active(True)
+        self.ap.config(essid="ESP32-Webconfig", authmode=4, password="12345678")  # authmode=network.AUTH_WPA_WPA2_PSK=4
+
+    def checkwifi(self):
+        if not self.station.isconnected():
+            self.station.disconnect()
+        # if ap.isconnected()
+
+    def configwifi(self, sid, pwd):
+        self.station.disconnect()
+        self.station.connect(sid, pwd)
+        try:
+            for s in range(0, 50):  # i 0-49
+                if not self.station.isconnected():
+                    time.sleep(0.1)
+                else:
+                    print("Connection successful")
+                    self.savewifi(sid, pwd)
+                    # print(station.ifconfig())
+                    return True
+        finally:
+            self.network_config = self.station.ifconfig()
+            self.ip, self.mask, self.gw, self.dns = self.station.ifconfig()
+        self.station.disconnect()
+        print("out of 5s connect fail......")
+        return False
+
+    def firstload(self):
+        rwjson(True)  # 读取json文件里的数据
+        self.station.active(True)
+        ssids = self.station.scan()
+        for i in config['network']:
+            for j in ssids:
+                if j[0].decode() == i['ssid']:
+                    if self.configwifi(i['ssid'], i['password']):
+                        return
+
+    def savewifi(self, sid, pwd):
+        global config
+        for i in config['network']:
+            if sid == i['ssid']:
+                i['password'] = pwd
+                rwjson(False)  # 写入json
+                return
+        config['network'].append({'ssid': sid, 'password': pwd})
+        rwjson(False)
+
+
+class Oled:
+    i2c = machine.I2C(scl=machine.Pin(15), sda=machine.Pin(4), freq=100000)
+    oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+    tim1 = machine.Timer(1)
+
+    def __init__(self):
+        pass
+        # self.tim1.init(period=3000, mode=machine.Timer.PERIODIC, callback=self.refresh_oled)  # 3秒刷新一次oled
+
+    def refresh_oled(self, network_config):
+        h, t = releasedht()
+        ip, mask, gw, dns = network_config
+        try:
+            self.oled.fill(0)
+            self.oled.text("I" + ip, 0, 0, 1)
+            self.oled.text("G" + gw, 0, 9, 1)
+            self.oled.text("Humidity:" + str(h) + "%", 0, 18, 1)
+            self.oled.text("Temperature:" + str(t) + "'C", 0, 27, 1)  # Celsius ℃
+            self.oled.show()
+        except Exception as e:
+            print('Oled:', e)
+
+
 def rwjson(c):
-    global conf
+    global config
     try:
         if c:
             with open('config.json', "r") as f:
-                conf = json.loads(f.read())
+                config = json.loads(f.read())
             f.close()
         else:
             with open('config.json', "w") as f:
-                f.write(json.dumps(conf))
+                f.write(json.dumps(config))
             f.close()
     except Exception:
         pass
 
 
-def savewifi(sid, pwd):
-    global conf
-    for i in conf['network']:
-        if sid == i['ssid']:
-            i['password'] = pwd
-            rwjson(False)  # 写入json
-            return
-    conf['network'].append({'ssid': sid, 'password': pwd})
-    rwjson(False)
-
-
 def savehttpRequest(host, port):
-    global conf
-    httpRequestDir['host'] = host
-    httpRequestDir['Port'] = port
-    conf['httpRequest'] = httpRequestDir
+    global config
+    httpRequest_config['host'] = host
+    httpRequest_config['Port'] = port
+    config['httpRequest_config'] = httpRequest_config
     rwjson(False)
 
 
